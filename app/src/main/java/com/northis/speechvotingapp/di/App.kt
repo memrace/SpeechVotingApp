6aa610f69abd243ad754381626ef0820b89430b9
Alexandr <sozonovalexander13@gmail.com>
2021-01-08 03:35:29 +0300

    OAuth 2.0
    Получение, хранение, обновление токена.

---
+++ app/src/main/java/com/northis/speechvotingapp/di/App.kt
@@ -0,0 +1,6 @@
+package com.northis.speechvotingapp.di
+
+import android.app.Application
+
+class App: Application() {
+}
--- app/src/main/java/com/northis/speechvotingapp/view/voting/VotingActivity.kt
+++ app/src/main/java/com/northis/speechvotingapp/view/voting/VotingActivity.kt
@@ -1,11 +1,13 @@
 package com.northis.speechvotingapp.view.voting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.northis.speechvotingapp.R
 import com.northis.speechvotingapp.databinding.ActivityVotingBinding
+import com.northis.speechvotingapp.view.authorization.AuthActivity
 import com.northis.speechvotingapp.view.ui.ActivityUIService
 
 
@@ -24,6 +26,9 @@ class VotingActivity : AppCompatActivity() {
         navController = Navigation.findNavController(this, R.id.nav_voting)
         activityUiService = ActivityUIService(this, mBinding.inclTopAppBar.topAppBar, mBinding.inclBottomNavigationBar.bottomNavigation, "Голосование", R.id.nav_voting)
         activityUiService.setActivityUi()
+        mBinding.button.setOnClickListener {
+            startActivity(Intent(this, AuthActivity::class.java))
+        }
     }
 
 }
